ea3df792de55835d938ab447e5a0df8cbe16fab4
Solastech <solomon.n@toosharptech.com>
2024-08-04 12:42:13 +0300

    push both side

---
+++ Server-Side/models/User.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  fullName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  phone: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: false,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  role: {
+    type: String,
+    enum: ['ATTENDEE', 'VENDOR'],
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('User', UserSchema);
---
+++ Server-Side/routes/auth.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const router = express.Router();
+const { signup,signin ,getUserProfile, updateUserProfile  } = require('../controllers/authController');
+
+// @route    POST api/auth/signup
+// @desc     Register user
+// @access   Public
+router.post('/signup', signup);
+// @route    POST api/auth/signin
+// @desc     Authenticate user & get token
+// @access   Public
+router.post('/signin', signin);
+// @route    GET api/user/profile
+// @desc     Get user profile
+// @access   Private
+router.get('/profile', getUserProfile);
+
+// @route    PUT api/user/profile
+// @desc     Update user profile
+// @access   Private
+router.put('/Profile',  updateUserProfile);
+
+module.exports = router;
