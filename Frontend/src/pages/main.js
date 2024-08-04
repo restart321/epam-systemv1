b6b0459a048da2a46db78a8916798e83b05803f2
Solastech <solomon.n@toosharptech.com>
2024-08-05 00:01:17 +0300

    main page

--- Frontend/src/pages/main.js
+++ Frontend/src/pages/main.js
@@ -19,7 +19,7 @@ const Main = () => {
         slidesToShow: 4,
         slidesToScroll: 1,
         autoplay: true,
-        autoplaySpeed: 2000,
+        autoplaySpeed: 1450,
         responsive: [
             {
                 breakpoint: 1024,
